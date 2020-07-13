910f711742e42e891d04446d02b1375bbf3bb768
Shaun Price <46876579+shaundnz@users.noreply.github.com>
2020-07-13 18:40:40 +1200

    Added GUI functionality - implemented JFileChooser and ActionListener's for buttons

--- src/ascii_art/ImageFileChooser.java
+++ src/ascii_art/ImageFileChooser.java
@@ -1,8 +1,15 @@
 package ascii_art;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
 
 public class ImageFileChooser extends JPanel{
     private final JFileChooser fc;
@@ -11,6 +18,7 @@ public class ImageFileChooser extends JPanel{
 
     public ImageFileChooser(){
         this.fc = new JFileChooser();
+        fc.setFileFilter(new FileNameExtensionFilter("Image files", ImageIO.getReaderFileSuffixes()));
         setLayout(new GridLayout(0,1));
 
         textField = new JLabel("Select an image file", SwingConstants.CENTER);
@@ -25,10 +33,43 @@ public class ImageFileChooser extends JPanel{
             setLayout(new FlowLayout());
 
             openFileButton = new JButton("Open file");
+            openFileButton.addActionListener(new OpenFileButtonActionListener());
             add(openFileButton);
 
             quitButton = new JButton("Quit");
+            quitButton.addActionListener(new QuitButtonActionListener());
             add(quitButton);
+        }
+    }
+
+    private class QuitButtonActionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            System.exit(0);
+        }
+    }
+
+    private class OpenFileButtonActionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            // Open the file chooser panel, check if file is chosen, then get the file
+            int returnVal = fc.showOpenDialog(null);
+            if (returnVal == JFileChooser.APPROVE_OPTION){
+                File file = fc.getSelectedFile();
+                BufferedImage image;
+                // Check if file is image
+                try{
+                    image = ImageIO.read(file);
+                    if (image == null){
+                        throw new IOException();
+                    }
+                    // If the file is an image, process it in a new thread
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, "Error reading image");
+                }
+            }
         }
     }
 }
