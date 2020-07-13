f5a6d085efabf56354704e8eb2e662ecb8d70609
Shaun Price <46876579+shaundnz@users.noreply.github.com>
2020-07-13 16:20:10 +1200

    ascii-main-branch initial commit

--- src/ascii_art/Pixel.java
+++ src/ascii_art/Pixel.java
@@ -1,9 +1,26 @@
 package ascii_art;
 
 public class Pixel {
-    private double red;
-    private double green;
-    private double blue;
+    private final double red;
+    private final double green;
+    private final double blue;
 
+    public Pixel(double r, double g, double b){
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
 
+    // Getters
+    public double getRed() {
+        return red;
+    }
+
+    public double getGreen() {
+        return green;
+    }
+
+    public double getBlue() {
+        return blue;
+    }
 }
