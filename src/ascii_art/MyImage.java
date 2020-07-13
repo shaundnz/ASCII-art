bae2d2468254835da105e28cd072bc0d1e0c4f79
Shaun Price <46876579+shaundnz@users.noreply.github.com>
2020-07-13 16:31:03 +1200

    Added MyImage class

---
+++ src/ascii_art/MyImage.java
@@ -0,0 +1,13 @@
+package ascii_art;
+
+
+import java.awt.image.BufferedImage;
+
+public class MyImage {
+
+    private BufferedImage image;
+
+    public MyImage(BufferedImage image){
+        this.image = image;
+    }
+}
